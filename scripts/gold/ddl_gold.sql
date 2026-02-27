db83cce1028542d96c216fb515c922ab8219fca1
dhriti18 <dhritishambhavi@gmail.com>
2026-02-27 16:35:04 +0000

    ddl_gold_sql

--- scripts/gold/ddl_gold.sql
+++ scripts/gold/ddl_gold.sql
@@ -31,23 +31,6 @@ ON ca.cid = la.cid
 -- Since we have repetitive and inconsistent data we need to decide which source system is the master
 -- As per the scope of the prject the crm system is the master data hence it has more accurate consistent data
 
-SELECT DISTINCT -- Data Integration
-    ci.cst_gndr,
-    ca.gen,
-    CASE WHEN ci.cst_gndr != 'n/a' THEN ci.cst_gndr
-            ELSE COALESCE(ca.gen, 'n/a')-- if the data is gen is also null then the gender information is n/a
-    END AS new_gen
-FROM DataWarehouse.silver.crm_cust_info ci -- giving an alias
-LEFT JOIN DataWarehouse.silver.erp_cust_az12 ca
-ON ci.cst_key = ca.cid
-LEFT JOIN DataWarehouse.silver.erp_loc_a101 la  
-ON ca.cid = la.cid
-ORDER BY 1,2
--- Since we have repetitive and inconsistent data we need to decide which source system is the master
--- As per the scope of the prject the crm system is the master data hence it has more accurate consistent data
-
-SELECT DISTINCT gender FROM gold.dim_customer -- quality checks
-
 
 -- creating product object
 -- This is another dimesion table as it holds descriptive information about the products
